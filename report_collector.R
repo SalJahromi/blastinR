21a670a3441bcb65b6655b901231842f3c19fedf
SJ-Tech <106458867+SalJahromi@users.noreply.github.com>
2024-08-07 21:20:36 -0700

    Updated report_collector.R to accomodate messages from create blast db and list output files from retrieve hit seqs. Needs testing.

--- report_collector.R
+++ report_collector.R
@@ -10,26 +10,75 @@ library(rmarkdown)
 
 timeStamp_global <- format(Sys.time(), "%Y%m%d%H%M%S")
 entry_time <- timestamp(stamp = date(), prefix = "--- ", suffix = " ---", quiet = FALSE)
-function_call_sig <- c('function_call')
+# function_call_sig <- c('function_call')
 
 
 blast_func <- blstinr('blastx','spike_protein_seqs_SARS','genomes_seqs_SARS.fasta', TRUE)
 plot1 <- summerize_bl(df1, blast_func, "ID", c("WatchMovie", "Time","Price"))
 
 
+# creates labels for the R markdown chunks
+label_generator <- function(){
+  return(paste0(UUIDgenerate()))
+}
 
+
+# Fixes the format of the function call object for the report
+fix_functionCall <- function(function_call){
+  function_call <- deparse(function_call)
+  function_call <- paste(function_call, collapes = " ")
+  function_call <- toString(function_call)
+  function_call <- gsub("\\\\",",",function_call)
+  function_call <- gsub("\\s*,\\s*",",", function_call)
+  function_call <- gsub("\\s+"," ", function_call)
+  return(function_call)
+}
+
+# Creates a report in rmd.
+#Parameters:
+# function_call: the function call or the line of code that ran a function by the user
+# data_list: A list that contains data from the function ran by the user
+#           contents of the list can be paths to csv or html files, or message and files outputted by function.
+#           If the function doesn't have those items, then they will have the NULL value in the list
+# entry time: The time the function was run by the user
 reporter_function <- function(function_call, data_list, entry_time){
   
-  rmd_file <- "history_markdown_graph_test.rmd" # name of rmd file
-  
+  function_call <- fix_functionCall(function_call)
+  rmd_file <- "blast_history_report.rmd" # name of rmd file
+  if(!file.exists(rmd_file)){
+    file.create(rmd_file)
+    title <- c(
+      "---",
+      "title: \"BLAST+ History\"",
+      "output: html_document",
+      "---",
+      "\n"
+    )
+    writeLines(title, rmd_file)
+  }
   rmd_content <- readLines(rmd_file)
   
   rmd_content_time <- c(paste0("### ", entry_time))
   rmd_content_new <- c()
+  
+  # For printing the make_blast_db message
+  
+  if(!is.null(data_list$message)){
+    make_db_content <- c(
+      paste0("#### Function Name: make_blast_db"),
+      paste0(data_list$message)
+    )
+    rmd_content_new <- c(make_db_content)
+  }
+  
+  
   # The time stamp of the entry
   if(!is.null(data_list$data_table)){
     table_content <- c(
-      paste0(cat(data_list$function_call)),
+      paste0("#### Function Name: blstinr"),
+      paste0("```{r ", label_generator(),",eval = FALSE}\n",
+             function_call,"\n",
+             "```"),
       paste0("```{r ", label_generator(),", echo=FALSE,warning = FALSE}\n",
              "library(DT)\n",
              "data <- read.csv('",data_list$data_table,"')\n",
@@ -43,12 +92,26 @@ reporter_function <- function(function_call, data_list, entry_time){
   
   # if plot_table is not null, then it contains a plot
   if(!is.null(data_list$plot_table)){
-    plot_content<- c(paste0("```{r ",label_generator(),", echo=FALSE , out.width = '80%', warning = FALSE}","\n",
+    plot_content<- c(
+      paste0("#### Function Name: blstinr"),
+      paste0("```{r ", label_generator(),",eval = FALSE}\n",
+             function_call,"\n",
+             "```"),
+      paste0("```{r ",label_generator(),", echo=FALSE , out.width = '80%', warning = FALSE}","\n",
                             paste0("knitr::include_url('",data_list$plot_table,"')"),"\n",
                             "```"),
                      ""
     )
     rmd_content_new <- c(plot_content)
+  }
+  
+  if(!is.null(data_list$output_files)){
+    output_content <- c(
+      paste0("#### Function Name: retrieve_hit_seqs"),
+      paste0(data_list$output_files)
+    )
+    
+    rmd_content_new <- c(output_content)
   }
   
   # check if the rmd file exists
@@ -69,29 +132,8 @@ reporter_function <- function(function_call, data_list, entry_time){
     # write in the new concatenation
     writeLines(update_content, rmd_file)
     
-  } else {
-    # title of the page
-    title <- c(
-      "---",
-      "title: \"BLAST+ History\"",
-      "output: html_document",
-      "---",
-      "\n"
-    )
-    
-    # create the file and write the title
-    writeLines(title, rmd_file)
-    
-    # read the file created
-    rmd_content <- readLines(rmd_file)
-    
-    # combine the title with the new content
-    update_content <- c(rmd_content, table_content)
-    
-    # write the combined content to the file
-    writeLines(update_content, rmd_file)
-  }
-  
+  } 
+
 }
 
 
